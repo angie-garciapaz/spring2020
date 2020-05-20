803cf0b83ae3c3f0c29176c1a9efe39fa615ee05
Angie Garcia-Paz <agarpaz06@gmail.com>
2020-05-19 23:22:56 -0600

    week 2

---
+++ src/app/contacts/contact.model.ts
@@ -0,0 +1,11 @@
+export class Contact {
+  constructor(
+    public contactId: number,
+    public name: string,
+    public email: string,
+    public phone: string,
+    public imageUrl: string,
+    public group: number
+  ) {
+  }
+}
